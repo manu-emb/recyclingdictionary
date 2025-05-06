77e163d4c1fdd531c96c33574a69214df4edf259
manu-emb <johneq1999@outlook.com>
2025-05-06 17:22:26 -0500

    Set Workbox to ignore url parameters for search.html and result.html. Cached them on service worker installation so that they work without being loaded first.

--- sw.js
+++ sw.js
@@ -11,21 +11,43 @@ workbox.precaching.precacheAndRoute([
     { url: '/result.html', revision: '1'},
     { url: 'recyclingitems.json', revision: '1' },
 ]);
+
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.CacheFirst()
 );
+// Function to create a cache key without specific query parameters
+const cacheKeyWillBeUsed = ({ request }) => {
+    const url = new URL(request.url);
+    // Remove the query parameter 'q'
+    url.searchParams.delete('q');
+    return url.toString();
+  };
+
+// Install event to precache search.html & result.html in "my-cache"
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('result-cache').then((cache) => {
+        return cache.addAll([
+          '/search.html',
+          '/result.html'
+        ]);
+      })
+    );
+  });
+
+// Register a route with caching strategy
 workbox.routing.registerRoute(
-    ({url}) => url.pathname === '/result.html',
-    new workbox.strategies.NetworkFirst({
+    ({ url }) => url.pathname === '/search.html' || url.pathname === '/result.html',
+    new workbox.strategies.StaleWhileRevalidate({
         cacheName: 'result-cache',
         plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [200],
+            }),
             {
-                cacheWillUpdate: async ({request, response}) => {
-                    return response;
-                },
+                cacheKeyWillBeUsed,
             },
         ],
-        ignoreSearch: true
     })
 );
