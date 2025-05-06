11f5b6e34a5aa03ebf51ecfcbdef7dae945f791b
manu-emb <johneq1999@outlook.com>
2025-05-06 09:33:02 -0500

    Some work on PWA

--- sw.js
+++ sw.js
@@ -1,7 +1,31 @@
 importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
+// Precache essential files
+workbox.precaching.precacheAndRoute([
+    { url: '/', revision: '1' },
+    { url: '/index.html', revision: '1' },
+    { url: '/homepage.css', revision: '1' },
+    { url: '/homepage.js', revision: '1' },
+    { url: '/search.html', revision: '1' },
+    { url: '/result.html', revision: '1'},
+    { url: 'recyclingitems.json', revision: '1' },
+]);
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst()
+    new workbox.strategies.CacheFirst()
+);
+workbox.routing.registerRoute(
+    ({url}) => url.pathname === '/result.html',
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'result-cache',
+        plugins: [
+            {
+                cacheWillUpdate: async ({request, response}) => {
+                    return response;
+                },
+            },
+        ],
+        ignoreSearch: true
+    })
 );
